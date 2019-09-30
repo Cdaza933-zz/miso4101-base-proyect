86b280c3b94118816670c54c1d08a12caa498416
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2019-09-29 19:23:28 -0500

    Creacion de modelo para actividades

--- activities/models.py
+++ activities/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
+
 # Create your models here.
+class Actividad(models.Model):
+    nombre = models.CharField(max_length=30)
+    numeroDeIntentos = models.IntegerField(default=0)
+    tieneRetroalimentacion = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Calificacion(models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    actividad = models.ForeignKey(Actividad, on_delete=models.CASCADE)
+    calificacion = models.DecimalField(max_digits=10, decimal_places=2)
+    retroalimentacion = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.calificacion
+
+
+class Pregunta(models.Model):
+    Pregunta = models.CharField(max_length=200)
+    actividad = models.ForeignKey(Actividad, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.calificacion
+
+
+class Respuestmultiple(models.Model):
+    respuesta = models.CharField(max_length=200)
+    esCorrecta = models.BooleanField()
+    preguntaSeleccionMultiple = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.calificacion
+
+
+class RespuestmultipleEstudiante(models.Model):
+    seleccionada = models.BooleanField()
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    preguntaSeleccionMultiple = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.calificacion
+
+
+class RespuestaAbiertaEstudiante(models.Model):
+    respuesta = models.CharField(max_length=200)
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    preguntaAbierta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.respuesta
+
+
+class RespuestaVoF(models.Model):
+    esCorrecta = models.BooleanField()
+    preguntaVoF = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.calificacion
+
+
+class RespuestaEstudianteVoF(models.Model):
+    respuesta = models.BooleanField()
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    preguntaVoF = models.ForeignKey(RespuestaVoF, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.respuesta
